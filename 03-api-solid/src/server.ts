9d792c18053eae99de652dae008c36c394db697e
Paulo Geliandes <geliandes@hotmail.com>
2024-10-21 09:26:33 -0300

    create server and integrate prisma

--- 03-api-solid/src/server.ts
+++ 03-api-solid/src/server.ts
@@ -1,11 +1,15 @@
-import { app } from './app';
+import { app } from "./app";
+import { env } from "./env";
 
-app.listen({
-  host: '0.0.0.0',
-  port: 3333,
-}).then(() => {
-  console.log('Server is running on port 3333');
-}).catch((error) => {
-  console.error(error);
-  process.exit(1);
-})
+app
+  .listen({
+    host: "0.0.0.0",
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log("🚀 Server is running on port 3333");
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
